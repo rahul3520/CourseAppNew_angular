7ca1ed9d65256bacc89ff605c871de64aa077fef
rahulg3520 <rahul@nestgroup.net>
2022-12-06 17:52:05 +0530

    add course

---
+++ src/app/add-course/add-course.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { ApiServiceService } from '../api-service.service';
+
+@Component({
+  selector: 'app-add-course',
+  templateUrl: './add-course.component.html',
+  styleUrls: ['./add-course.component.css']
+})
+export class AddCourseComponent {
+
+  courseTitle=""
+  courseDescription=""
+  courseDuration=""
+  courseDate=""
+  courseVenue=""
+
+  constructor(private api:ApiServiceService){}
+
+  readCourse=()=>
+  {
+    let data:any={"courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"courseVenue":this.courseVenue,"courseDuration":this.courseDuration,"courseDate":this.courseDate}
+
+    console.log(data)
+
+    this.api.addCourse(data).subscribe(
+
+      (response:any)=>
+      {
+        console.log(response)
+
+        if(response.status=="success")
+        {
+            alert("course addeed successfully");
+            this.courseTitle=""
+            this.courseDescription=""
+            this.courseDuration=""
+            this.courseDate=""
+            this.courseVenue=""
+
+        }
+        else
+        {
+            alert("something went wrong")
+        }
+      }
+
+    )
+
+
+
+  }
+
+  
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,22 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { FormsModule } from '@angular/forms';
 
-
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    AddCourseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule
     
   ],
   providers: [],
