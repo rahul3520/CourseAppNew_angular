ab50da0a0b7eabe52713b74dc5507a5ede3f3e27
rahulg3520 <rahul@nestgroup.net>
2022-12-07 17:14:45 +0530

    search api integration

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -17,4 +17,9 @@ export class ApiServiceService {
   {
     return this.http.get("http://localhost:8080/view")
   }
+
+  searchCourse=(dataToSend:any)=>
+  {
+    return this.http.post("http://localhost:8080/search",dataToSend)
+  }
 }
